834ca368f5daa6341ed6b8463f0128d95d0a79b6
LM GOH <drakgoh.5610@gmail.com>
2020-10-15 18:46:54 +0800

    Manager_Chac Scene Update
    Added self rotation code for navigation arrows

--- Assets/Scripts/FollowChac.cs
+++ Assets/Scripts/FollowChac.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NameFollowChac : MonoBehaviour
+public class FollowChac : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
---
+++ Assets/Scripts/SelfRotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CompareTag("NavArrow"))
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * 40.0f);
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+
+        
+    }
+}
