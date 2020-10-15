baccfad62eed1b76e2676377686209b5a459ff5d
LM GOH <drakgoh.5610@gmail.com>
2020-10-16 00:51:00 +0800

    Incomplete Floating and NavManager script

--- Assets/Scripts/NavigationManager.cs
+++ Assets/Scripts/NavigationManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class NavigationManager : MonoBehaviour
 {
+
+    private GameObject chacObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        chacObject = GameObject.Find("Manager 1_Chac");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (chacObject.CompareTag("Character"))
+        {
+            chacObject.transform.Translate(Vector3.left * Time.deltaTime);
+        }
     }
 }
