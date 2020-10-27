68fb1f420820acfff7b820c923dec087e55eef1c
Seba-A <aspinallsebastian@gmail.com>
2020-10-27 20:56:49 +0100

    wow

---
+++ Assets/Scripts/In_Game_Layout/DisappearPanel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DisappearPanel : MonoBehaviour
+{
+    public GameObject popUp;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                if (popUp && !OverUIObject())
+                {
+                    //Debug.Log("Not UI");
+                    popUp.SetActive(false);
+                }
+            }
+        }
+    }
+
+    //to check whether we are touching UI elements
+    private bool OverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
+    }
+}
