673e32758935a0831a74a229933b1a19af5a3fe6
Seba-A <aspinallsebastian@gmail.com>
2020-10-27 17:02:27 +0100

    player and popup window

---
+++ Assets/Scripts/In_Game_Layout/ClickingOnTeam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickingOnTeam : MonoBehaviour
+{
+    public GameObject stats;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                if (hitInfo.collider.gameObject.tag == "Team")
+                {
+                    Debug.Log(gameObject.tag);
+                    stats.SetActive(true);
+                }
+            }
+        }
+    }
+}
