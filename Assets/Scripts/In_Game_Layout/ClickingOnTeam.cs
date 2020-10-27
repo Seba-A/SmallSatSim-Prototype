e345f0f7bed25ceb93646adcf189e6493c560c84
Seba-A <aspinallsebastian@gmail.com>
2020-10-27 20:30:04 +0100

    Made the Impossible
    the impossible has been done, now you can close the pup up by clicking anywhere that is not UI

--- Assets/Scripts/In_Game_Layout/ClickingOnTeam.cs
+++ Assets/Scripts/In_Game_Layout/ClickingOnTeam.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ClickingOnTeam : MonoBehaviour
 {
@@ -15,12 +16,26 @@ public class ClickingOnTeam : MonoBehaviour
             RaycastHit hitInfo;
             if (Physics.Raycast(ray, out hitInfo))
             {
-                if (hitInfo.collider.gameObject.tag == "Team")
+                if (hitInfo.collider.gameObject.tag == "Teammate")
                 {
-                    Debug.Log(gameObject.tag);
                     stats.SetActive(true);
+                }
+                else if (stats && !OverUIObject())
+                {
+                    //Debug.Log("Not UI");
+                    stats.SetActive(false);
                 }
             }
         }
+    }
+
+    //to check whether we are touching UI elements
+    private bool OverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
     }
 }
---
+++ Assets/Scripts/In_Game_Layout/DisappearPanelBAD.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DisappearPanelBAD : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.gameObject.SetActive(ClickingSelfOrChild());
+        }
+    }
+    private bool ClickingSelfOrChild()
+    {
+        RectTransform[] rectTransforms = GetComponentsInChildren<RectTransform>();
+        foreach (RectTransform rectTransform in rectTransforms)
+        {
+            if (EventSystem.current.currentSelectedGameObject == rectTransform.gameObject)
+            {
+                return true;
+            };
+        }
+        return false;
+    }
+}
