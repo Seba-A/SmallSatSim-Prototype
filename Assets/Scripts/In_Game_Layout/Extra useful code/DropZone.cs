35fec3c7d6e18565add93d1a060766adb9b63304
Seba-A <aspinallsebastian@gmail.com>
2020-10-30 23:21:35 +0100

    Now you can assign tasks
    I fucken did it, Hell yeah bitcheeeeesss. There is a small bug (you can place more than one task in the same slot) but other than that it works deliciously

---
+++ Assets/Scripts/In_Game_Layout/Extra useful code/DropZone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+
+        DragUI d = eventData.pointerDrag.GetComponent<DragUI>();
+        if(d != null)
+        {
+            d.parentToReturnTo = this.transform;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //Debug.Log("OnPointerEnter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //Debug.Log("OnPointerExit");
+    }
+}
