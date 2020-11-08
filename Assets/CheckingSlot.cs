d90ff5b74124c3d71ce6d36d47183569b9c38192
Seba-A <aspinallsebastian@gmail.com>
2020-11-08 15:36:29 +0100

    part 4

---
+++ Assets/CheckingSlot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckingSlot : MonoBehaviour
+{
+    public bool timerIsRunning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CheckSlot()
+    {
+        if (this.transform.parent.name == "ContentTask")
+        {
+            Debug.Log("Task has been assigned");
+        }
+        else
+        {
+            Debug.Log("No task has been assigned");
+        }
+    }
+}
