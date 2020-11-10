1b8dd182cf930ccbbc2089042202ace524655e7f
LM GOH <drakgoh.5610@gmail.com>
2020-11-10 12:15:33 +0800

    Updated Character Profiles in Prefabs

---
+++ Assets/Scripts/In_Game_Layout/ManagerAndTeam/CharacterInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[CreateAssetMenu(fileName = "NewCharacterInfo", menuName = "CharacterInfo")]
+public class CharacterInfo : ScriptableObject
+{
+    // Character Profile
+    public new string name;
+    public TextMeshProUGUI descriptionText;
+
+    public int speed;
+    public int quality;
+    public int relationship;
+    public int focus;
+    public int creativity;
+
+    public string trait;
+    public string field;
+
+    public TextMeshProUGUI leaderEffect;
+
+
+    // Character Role(s) and Task(s)
+    public string role;
+    public TextMeshProUGUI roleDescription;
+
+}
