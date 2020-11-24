63503fad407dd49f00b05987617a03c99285e3b4
Seba-A <aspinallsebastian@gmail.com>
2020-11-24 14:55:50 +0100

    Move tasks up and down in the list

---
+++ Assets/Scripts/In_Game_Layout/TaskUpDown.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskUpDown : MonoBehaviour
+{
+    int task_IndexNumber;
+    int numberOfTasksInList;
+
+    // Update is called once per frame
+    void Update()
+    {
+        task_IndexNumber = transform.GetSiblingIndex();
+        transform.SetSiblingIndex(task_IndexNumber);
+        //Debug.Log(task_IndexNumber);
+
+        numberOfTasksInList = transform.parent.transform.childCount - 1;
+    }
+
+    public void IncreaseIndex()
+    {
+        if (task_IndexNumber < numberOfTasksInList)
+        {
+            task_IndexNumber++;
+            transform.SetSiblingIndex(task_IndexNumber);
+        }
+    }
+
+    public void DecreaseIndex()
+    {
+        if (task_IndexNumber >= 1)
+        {
+            task_IndexNumber--;
+            transform.SetSiblingIndex(task_IndexNumber);
+        }
+    }
+}
