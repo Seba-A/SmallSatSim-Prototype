9328f0508d00e72266a9c486b9bf4ccf35e70f01
LMGOH <drakgoh.5610@gmail.com>
2020-11-25 15:12:42 +0800

    Completed role saving function

--- Assets/Scripts/In_Game_Layout/ManagerAndTeam/RoleSelection.cs
+++ Assets/Scripts/In_Game_Layout/ManagerAndTeam/RoleSelection.cs
@@ -9,33 +9,21 @@ public class RoleSelection : MonoBehaviour
 {
     public GameObject memberAttached;
     private GameObject roleUIPanel;
+
     public Dropdown primaryDropdown;
     public Dropdown secondaryDropdown;
 
+    public TextMeshProUGUI primaryRoleDescription;
+    public TextMeshProUGUI secondaryRoleDescription;
+
     public TextMeshProUGUI[] listOfRoles;
 
+    /* generic key defined for character roles
+     * For the unique key for each character, will need to add the UIPanel.name + generic key string
+     */
     private readonly string primaryRole = " PrimaryRoleOption";
     private readonly string secondaryRole = " SecondaryRoleOption";
 
-    public void SavingRoles()
-    {
-
-        primaryDropdown.onValueChanged.AddListener(new UnityEngine.Events.UnityAction<int>(index =>
-        {
-            PlayerPrefs.SetInt(roleUIPanel.name + primaryRole, primaryDropdown.value);
-            PlayerPrefs.Save();
-        }));
-        Debug.Log(primaryDropdown.value + " is selected.");
-
-
-        secondaryDropdown.onValueChanged.AddListener(new UnityEngine.Events.UnityAction<int>(index =>
-        {
-            PlayerPrefs.SetInt(roleUIPanel.name + secondaryRole, secondaryDropdown.value);
-            PlayerPrefs.Save();
-        }));
-        Debug.Log(secondaryDropdown.value + " is selected.");
-
-    }
 
     // Start is called before the first frame update
     void Start()
@@ -48,4 +36,41 @@ public class RoleSelection : MonoBehaviour
         Debug.Log("Displaying secondary role: " + secondaryDropdown.value);
     }
 
+    public void SavePrimaryRole()
+    {
+        // player preference key is the combination of UIpanel name plus the generic key
+        PlayerPrefs.SetInt(roleUIPanel.name + primaryRole, primaryDropdown.value);
+        PlayerPrefs.Save();
+        Debug.Log(primaryDropdown.value + " is selected.");
+        Debug.Log("Primary Role of " + memberAttached.name + " is now: " + primaryDropdown.captionText.text);
+
+        /*
+        // show the relevant role effects
+        foreach (TextMeshProUGUI element in listOfRoles)
+        {
+            if (primaryDropdown.captionText.text == element.name.Substring(0, element.name.Length -12))
+            {
+                primaryRoleDescription.text = element.text;
+            }
+        }*/
+    }
+
+    public void SaveSecondaryRole()
+    {
+        // player preference key is the combination of UIpanel name plus the generic key
+        PlayerPrefs.SetInt(roleUIPanel.name + secondaryRole, secondaryDropdown.value);
+        PlayerPrefs.Save();
+        Debug.Log(secondaryDropdown.value + " is selected.");
+        Debug.Log("Secondary Role of " + memberAttached.name + " is now: " + secondaryDropdown.captionText.text);
+
+        /*
+        // show the relevant role effects
+        foreach (TextMeshProUGUI element in listOfRoles)
+        {
+            if (secondaryDropdown.captionText.text == element.name.Substring(0, element.name.Length - 12))
+            {
+                secondaryRoleDescription.text = element.text;
+            }
+        }*/
+    }
 }
