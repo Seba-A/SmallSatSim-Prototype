6336485fe2962e60cb6a458ec6a8c468baf348e5
Seba-A <aspinallsebastian@gmail.com>
2020-11-26 00:09:13 +0100

    new version of task assign
    Integrated the new version with an old one. Now we will be able to set specific actions to happen when you complete a task for the first time. Probably use this to see wheather the player has completed all of the tasks and if not give penalties in the calculation of the final score at the end of the mission

--- Assets/Scripts/In_Game_Layout/TaskTimer.cs
+++ Assets/Scripts/In_Game_Layout/TaskTimer.cs
@@ -51,6 +51,33 @@ public class TaskTimer : MonoBehaviour
         {
             time.text = "Time: " + timeRemaining.ToString("0");
 
+            if (FirstTimeCompletion)
+            {
+                if(timeRemaining > 0)
+                {
+                    TimerRunning();
+                }
+                else
+                {
+                    TimerZero();
+                }
+            }
+            else
+            {
+                if (timeRemaining > 0)
+                {
+                    TimerRunning();
+                }
+                else
+                {
+                    TimerZero();
+                    //to activate the tick on the task
+                    FirstTimeCompletion = true;
+                    FirstCompletionTick.SetActive(true);
+                }
+            }
+
+            /*
             if (timeRemaining > 0)
             {
                 timeRemaining -= Time.deltaTime;
@@ -81,7 +108,38 @@ public class TaskTimer : MonoBehaviour
                 RepeatPenalty = RepeatPenalty + IncreaseRepeatPenalty;
                 //Debug.Log(RepeatPenalty);
             }
+            */
         }
+    }
+
+    public void TimerRunning()
+    {
+        timeRemaining -= Time.deltaTime;
+        assignTaskPanel.GetComponent<Slottable_Assign>().startNextTask = false;
+        //slottable_Assign.startNextTask = false;
+    }
+
+    public void TimerZero()
+    {
+        //Debug.Log("Time has run out!");
+        timeRemaining = 0;
+        gameObject.GetComponent<TaskV2>().taskCompleted = true;
+        assignTaskPanel.GetComponent<Slottable_Assign>().startNextTask = true;
+        //slottable_Assign.startNextTask = true;
+
+        assignTaskPanel.GetComponent<Slottable_Assign>().tasks.Remove(gameObject.name);
+
+        AddScore(RepeatPenalty);
+        ScorePenalty();
+
+        //return task to task list
+        this.transform.SetParent(parentToReturnToOnceCompleted);
+
+        //reactivate the draggable thing
+        (parentToReturnToOnceCompleted.transform.Find(this.transform.name).GetComponent<Draggable>() as MonoBehaviour).enabled = true;
+
+        RepeatPenalty = RepeatPenalty + IncreaseRepeatPenalty;
+        //Debug.Log(RepeatPenalty);
     }
 
     //increase the score value by the task's indicated values
