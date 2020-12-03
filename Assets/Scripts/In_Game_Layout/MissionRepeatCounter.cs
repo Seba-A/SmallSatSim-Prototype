5a549aed709197dc9b0a8b23a1af3b326a13fcea
Seba-A <aspinallsebastian@gmail.com>
2020-12-03 12:34:35 +0100

    mission repeat counter script

---
+++ Assets/Scripts/In_Game_Layout/MissionRepeatCounter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionRepeatCounter : MonoBehaviour
+{
+    public int TimesMissionIsReapeated;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimesMissionIsReapeated = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void IncreaseRepeatCount()
+    {
+        TimesMissionIsReapeated++;
+    }
+}
